d81e05c4f4bc8076e20b8de991313d2083c9664e
Tom Holmes <tholmes59@gmail.com>
2019-02-21 22:45:59 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -6,3 +6,21 @@ CREATE TABLE projects (
   start_date TEXT,
   end_date TEXT 
 );
+
+CREATE TABLE projects (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date TEXT,
+  end_date TEXT 
+);
+
+CREATE TABLE projects (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date TEXT,
+  end_date TEXT 
+);
