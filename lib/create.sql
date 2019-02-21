f974a07e80d3988da5989a9037a099b612d91597
Tom Holmes <tholmes59@gmail.com>
2019-02-21 22:53:31 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,17 +1,17 @@
-CREATE TABLE projects (
-  id INTEGER PRIMARY KEY,
-  title TEXT,
-  category TEXT,
-  funding_goal INTEGER,
-  start_date TEXT,
-  end_date TEXT 
-);
-
--- CREATE TABLE users (
+-- CREATE TABLE projects (
 --   id INTEGER PRIMARY KEY,
---   name TEXT,
---   age INTEGER,
+--   title TEXT,
+--   category TEXT,
+--   funding_goal INTEGER,
+--   start_date TEXT,
+--   end_date TEXT 
 -- );
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+);
 
 -- CREATE TABLE pledges (
 --   id INTEGER PRIMARY KEY,
