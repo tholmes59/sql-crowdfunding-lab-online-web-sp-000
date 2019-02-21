842c405b29f808bfc0603c2c1ea9a8dd53012f29
Tom Holmes <tholmes59@gmail.com>
2019-02-21 23:06:32 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -46,3 +46,8 @@ INSERT INTO pledges (id, amount, user_id, project_id) VALUES
 (28, 90.00, 17, 9),
 (29, 230.00, 16, 6),
 (30, 450.00, 15, 5);
+
+
+def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
+"SELECT projects.title, SUM(pledges.amount) FROM projects INNER JOIN pledges ON projects.id = pledges.project_id"
+end
