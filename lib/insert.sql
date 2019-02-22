de22b0562d9948d28254a42a51daff6dd32c9c17
Tom Holmes <tholmes59@gmail.com>
2019-02-22 00:04:07 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -47,4 +47,6 @@ INSERT INTO pledges (id, amount, user_id, project_id) VALUES
 (29, 230.00, 16, 6),
 (30, 450.00, 15, 5);
 
-
+def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
+"Write your SQL query Here"
+end
